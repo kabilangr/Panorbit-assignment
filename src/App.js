5853f186b2c51f82cf913ac061975259e2324da8
kabilangr <g.r.kabilan@gmail.com>
2023-08-21 15:03:08 +0530

    router fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
+import {HashRouter as Router, Routes, Route, Navigate} from "react-router-dom"
 import './App.css';
 import { LandingPage } from "./components/LandingPage/LandingPage";
 import { Home } from "./components/Home/Home";
 
 function App() {
   return (
-    <Router basename="/react-dummy">
+    <Router>
       <div className="App">
         <Routes>
           <Route path="/" exact 
           // elements={<Navigate to="/home" replace={true}/>}
-          elements={<LandingPage/>} />
+          element={<LandingPage/>} />
           <Route path="/home" exact element={<Home/>}/>
         </Routes>
       </div>
