1e48cd540fe24f7d6569c0309ef037b80f2c3730
kabilangr <g.r.kabilan@gmail.com>
2023-08-21 14:45:46 +0530

    folder template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
 import './App.css';
+import { LandingPage } from "./components/LandingPage/LandingPage";
+import { Home } from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router basename="/react-dummy">
+      <div className="App">
+        <Routes>
+          <Route path="/" exact 
+          // elements={<Navigate to="/home" replace={true}/>}
+          elements={<LandingPage/>} />
+          <Route path="/home" exact element={<Home/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
